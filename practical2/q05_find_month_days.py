68ef22fd49bced029a5d58964c53d6c7db44fe7b
ddx <dai.tianle@dhs.sg>
2018-01-29 17:45:21 +0800

    Update q05_find_month_days.py

--- practical2/q05_find_month_days.py
+++ practical2/q05_find_month_days.py
@@ -8,21 +8,21 @@ def isLeap(a):
 
 month = int(input("Enter month: "))
 year = int(input("Enter year: "))
+A= ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 if 0<month<=12:
     if month == 2:
         if isLeap(year)== True:
-            print('NO OF DAYS IS',28)
+            print(A[month-1],year,'has',29,'days')
         else:#non-leap
-            print('NO OF DAYS IS',29)
+            print(A[month-1],year,'has',28,'days')
     elif month % 2== 0 and month <8 and month!=2:
-        print('NO OF DAYS IS',30)
+        print(A[month-1],year,'has',30,'days')
     elif month % 2== 0 and month >=8:
-        print('NO OF DAYS IS',31)
+        print(A[month-1],year,'has',31,'days')
     elif month % 2== 1 and month <9:
-        print('NO OF DAYS IS',31)
+        print(A[month-1],year,'has',31,'days')
     elif month % 2== 1 and month >=9:
-        print('NO OF DAYS IS',30)
+        print(A[month-1],year,'has',30,'days')
 else:# invalid month
     print("Invalid month hohoho")
-
