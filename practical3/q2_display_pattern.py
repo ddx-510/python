b42db01af948980726728e383d5a725d06558709
ddx <dai.tianle@dhs.sg>
2018-02-04 21:45:26 +0800

    Update q2_display_pattern.py

--- practical3/q2_display_pattern.py
+++ practical3/q2_display_pattern.py
@@ -2,9 +2,9 @@
 n = int(input("enter n"))
 
 for i in range (n,0,-1):
-    print(((i-1)*2)*" ",end=" ")
+    print((i-1)*"\t",end="")
     for j in range(n-i+1,0,-1):
-        print(j,end=" ")
+        print(j,end="\t")
     print()
 
 
